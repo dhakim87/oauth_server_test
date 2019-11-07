0539d9807d7aae627a4c9fe3fd2efd3cf69010b6
dhakim87 <dhakimdahakim@gmail.com>
2019-11-06 16:22:40 -0800

    Reordered sql commands so that tables are created before we try to add permissions for the oauthdb user

--- makeDB.sh
+++ makeDB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "CREATE DATABASE oauthdb" | psql
-echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO oauthdb" | psql -d oauthdb
 echo "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"" | psql -d oauthdb
 cat schema.sql | psql -d oauthdb
+echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO oauthdb" | psql -d oauthdb
 echo "insert into oauth_clients VALUES('123', 'pass123', 'https://example.com/callback');" | psql -d oauthdb
